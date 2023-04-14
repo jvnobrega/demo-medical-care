5479436b08c5f3d510a34785ba757c0439654e9e
Joao Victor Nobrega <joao.nobrega@fanduel.com>
2023-04-14 18:24:35 +0100

    Initial commit

---
+++ src/main/java/com/acme/demomedicalcare/DemoMedicalCareApplication.java
@@ -0,0 +1,13 @@
+package com.acme.demomedicalcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMedicalCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMedicalCareApplication.class, args);
+	}
+
+}
